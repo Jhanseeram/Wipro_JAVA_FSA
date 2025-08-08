0b1467d37f0b9f71f620666774f38f4a240a89d3
Jhansee Ram <110383533+Jhanseeram@users.noreply.github.com>
2025-08-08 22:38:32 +0530

    Intial Commit

--- 08-08-2025/08-08-2025/LetsGo/src/main/java/com/wipro/LetsGo/repo/VehicleRepo.java
+++
@@ -1,7 +0,0 @@
-package com.wipro.LetsGo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.wipro.LetsGo.entity.VehicleEntity;
-
-public interface VehicleRepo extends JpaRepository<VehicleEntity, Long> {
-}
