65ff2b8150e85962350f08b54c8788428055e558
Jhansee Ram <110383533+Jhanseeram@users.noreply.github.com>
2025-08-18 22:28:46 +0530

    Intial Commit

--- 16-08-2025/SpringBootEmployeeApp/src/main/java/com/example/employeeapp/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.employeeapp.repository;
-
-import com.example.employeeapp.entity.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
